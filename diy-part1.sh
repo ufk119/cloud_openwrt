d03592c3b2c16bce5d4d0c4efe12fe93fb41382a
Mark <55911453+ufk119@users.noreply.github.com>
2022-02-11 22:11:18 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -22,3 +22,4 @@
 
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+echo 'src-git kiddin9_pkg https://github.com/kiddin9/openwrt-packages' >>feeds.conf.default
