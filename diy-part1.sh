9527971086b059aace8f454a4b8babc905cb589e
Mark <55911453+ufk119@users.noreply.github.com>
2022-05-13 11:32:20 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,6 +19,9 @@
 
 
 
+echo 'src-git nas kenzo https://github.com/linkease/nas-packages.git' >>feeds.conf.default
+echo 'src-git nas_luci https://github.com/linkease/nas-packages-luci.git' >>feeds.conf.default
+
 
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
